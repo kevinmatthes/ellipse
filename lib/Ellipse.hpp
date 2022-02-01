30197f65e14a7f86cd588c60ec09809ee9825836
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-02-01 13:56:57 +0100

    Add settings to main header

--- lib/Ellipse.hpp
+++ lib/Ellipse.hpp
@@ -62,6 +62,22 @@ using std :: vector;
 
 
 
+/*
+ * Settings.
+ */
+
+#ifdef  __WINDOWS__
+#ifdef  __MAKE_DLL__
+#define EXPORT __declspec (dllexport)
+#else
+#define EXPORT __declspec (dllimport)
+#endif  // ! __MAKE_DLL__
+#else
+#define EXPORT
+#endif  // ! __WINDOWS__
+
+
+
 /**
  * \brief   A simple ellipse class.
  *

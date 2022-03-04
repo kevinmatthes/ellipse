d03ff1065cf7ba62ea163238afc136d64cc1bfd4
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-03-04 19:39:38 +0100

    Bugfix

--- lib/set_normal.cpp
+++ lib/set_normal.cpp
@@ -18,15 +18,15 @@
 
 /**
  * \author      Kevin Matthes
- * \brief       The setter for `Ellipse :: centre`.
+ * \brief       The setter for `Ellipse :: normal`.
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
- * \file        set_centre.cpp
+ * \file        set_normal.cpp
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  *
- * Since `centre` is a private attribute, it should be set exclusively using
+ * Since `normal` is a private attribute, it should be set exclusively using
  * this method.
  */
 
@@ -41,45 +41,45 @@
 
 
 /**
- * \brief   Reset this ellipse's centre.
+ * \brief   Reset this ellipse's normal.
  *
- * Set this ellipse's centre to `0.f, 0.f, 0.f`.
+ * Set this ellipse's normal to `0.f, 0.f, 0.f`.
  */
 
-inline void Ellipse :: set_centre (void)
+inline void Ellipse :: set_normal (void)
 {
-    this -> set_centre (0.f, 0.f, 0.f);
+    this -> set_normal (0.f, 0.f, 0.f);
     return;
 }
 
 
 
 /**
- * \brief   The setter method for `Ellipse :: centre`.
- * \param   centre  This ellipse's centre.
+ * \brief   The setter method for `Ellipse :: normal`.
+ * \param   normal  This ellipse's normal.
  *
- * Since `centre` is a private attribute, it should be set exclusively using
+ * Since `normal` is a private attribute, it should be set exclusively using
  * this method.
  */
 
-void Ellipse :: set_centre (const vector <float> & centre)
+void Ellipse :: set_normal (const vector <float> & normal)
 {
-    switch (centre.size ())
+    switch (normal.size ())
     {
         case 0x0:
-            this -> set_centre ();
+            this -> set_normal ();
             break;
 
         case 0x1:
-            this -> set_centre (centre[0x0], 0.f, 0.f);
+            this -> set_normal (normal[0x0], 0.f, 0.f);
             break;
 
         case 0x2:
-            this -> set_centre (centre[0x0], centre[0x1], 0.f);
+            this -> set_normal (normal[0x0], normal[0x1], 0.f);
             break;
 
         default:
-            this -> set_centre (centre[0x0], centre[0x1], centre[0x2]);
+            this -> set_normal (normal[0x0], normal[0x1], normal[0x2]);
             break;
     };
 
@@ -89,20 +89,20 @@ void Ellipse :: set_centre (const vector <float> & centre)
 
 
 /**
- * \brief   The setter method for `Ellipse :: centre`.
- * \param   x   This ellipse's centre's x component.
- * \param   y   This ellipse's centre's y component.
- * \param   z   This ellipse's centre's z component.
+ * \brief   The setter method for `Ellipse :: normal`.
+ * \param   x   This ellipse's normal's x component.
+ * \param   y   This ellipse's normal's y component.
+ * \param   z   This ellipse's normal's z component.
  *
- * Since `centre` is a private attribute, it should be set exclusively using
+ * Since `normal` is a private attribute, it should be set exclusively using
  * this method.
  */
 
-inline void Ellipse :: set_centre (const float x, const float y, const float z)
+inline void Ellipse :: set_normal (const float x, const float y, const float z)
 {
-    this -> centre[0x0] = x;
-    this -> centre[0x1] = y;
-    this -> centre[0x2] = z;
+    this -> normal[0x0] = x;
+    this -> normal[0x1] = y;
+    this -> normal[0x2] = z;
     return;
 }
 
--- lib/set_tangent.cpp
+++ lib/set_tangent.cpp
@@ -18,15 +18,15 @@
 
 /**
  * \author      Kevin Matthes
- * \brief       The setter for `Ellipse :: centre`.
+ * \brief       The setter for `Ellipse :: tangent`.
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
- * \file        set_centre.cpp
+ * \file        set_tangent.cpp
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  *
- * Since `centre` is a private attribute, it should be set exclusively using
+ * Since `tangent` is a private attribute, it should be set exclusively using
  * this method.
  */
 
@@ -41,45 +41,45 @@
 
 
 /**
- * \brief   Reset this ellipse's centre.
+ * \brief   Reset this ellipse's tangent.
  *
- * Set this ellipse's centre to `0.f, 0.f, 0.f`.
+ * Set this ellipse's tangent to `0.f, 0.f, 0.f`.
  */
 
-inline void Ellipse :: set_centre (void)
+inline void Ellipse :: set_tangent (void)
 {
-    this -> set_centre (0.f, 0.f, 0.f);
+    this -> set_tangent (0.f, 0.f, 0.f);
     return;
 }
 
 
 
 /**
- * \brief   The setter method for `Ellipse :: centre`.
- * \param   centre  This ellipse's centre.
+ * \brief   The setter method for `Ellipse :: tangent`.
+ * \param   tangent  This ellipse's tangent.
  *
- * Since `centre` is a private attribute, it should be set exclusively using
+ * Since `tangent` is a private attribute, it should be set exclusively using
  * this method.
  */
 
-void Ellipse :: set_centre (const vector <float> & centre)
+void Ellipse :: set_tangent (const vector <float> & tangent)
 {
-    switch (centre.size ())
+    switch (tangent.size ())
     {
         case 0x0:
-            this -> set_centre ();
+            this -> set_tangent ();
             break;
 
         case 0x1:
-            this -> set_centre (centre[0x0], 0.f, 0.f);
+            this -> set_tangent (tangent[0x0], 0.f, 0.f);
             break;
 
         case 0x2:
-            this -> set_centre (centre[0x0], centre[0x1], 0.f);
+            this -> set_tangent (tangent[0x0], tangent[0x1], 0.f);
             break;
 
         default:
-            this -> set_centre (centre[0x0], centre[0x1], centre[0x2]);
+            this -> set_tangent (tangent[0x0], tangent[0x1], tangent[0x2]);
             break;
     };
 
@@ -89,20 +89,20 @@ void Ellipse :: set_centre (const vector <float> & centre)
 
 
 /**
- * \brief   The setter method for `Ellipse :: centre`.
- * \param   x   This ellipse's centre's x component.
- * \param   y   This ellipse's centre's y component.
- * \param   z   This ellipse's centre's z component.
+ * \brief   The setter method for `Ellipse :: tangent`.
+ * \param   x   This ellipse's tangent's x component.
+ * \param   y   This ellipse's tangent's y component.
+ * \param   z   This ellipse's tangent's z component.
  *
- * Since `centre` is a private attribute, it should be set exclusively using
+ * Since `tangent` is a private attribute, it should be set exclusively using
  * this method.
  */
 
-inline void Ellipse :: set_centre (const float x, const float y, const float z)
+inline void Ellipse :: set_tangent (const float x, const float y, const float z)
 {
-    this -> centre[0x0] = x;
-    this -> centre[0x1] = y;
-    this -> centre[0x2] = z;
+    this -> tangent[0x0] = x;
+    this -> tangent[0x1] = y;
+    this -> tangent[0x2] = z;
     return;
 }
 
